3b6b85b5efe052824e0f2df6fea56dd8677e63d3
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2024-09-15 23:02:54 +0000

    Added Date class/modular date variables, added ToggleWait management option, WIP DateTick, Added Trades

--- LT-Trails.py
+++ LT-Trails.py
@@ -1,5 +1,5 @@
 from colorama import Fore
-import sys, time
+import sys, time, random
 
 class Characters:
     def __init__(self):
@@ -29,6 +29,7 @@ class Characters:
         self.names[0] = input('\nWhat is the name of the wagon leader? ')
 
 
+
 class Supplies():
     def __init__(self):
         self.oxen = 0
@@ -42,7 +43,39 @@ class Supplies():
 
 
 
-def MainMenu():
+class Date():
+    def __init__(self):
+        self.day = 1
+        self.month = ''
+        self.year = 1848
+        self.monthIndex = 3
+        self.monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        self.dayList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def updateMonth(self):
+        self.month = self.monthList[self.monthIndex]
+
+    def increaseDay(self, n, update=False):
+        for _ in range(n):
+            self.day += 1
+            if self.day > self.dayList[self.monthIndex]:
+                self.day = 1
+                self.increaseMonth()
+            if update == True:
+                if Wait == True:
+                    time.sleep(0.5)
+                print(self.month, str(self.day) + ',', self.year)
+
+    def increaseMonth(self):
+        self.monthIndex += 1
+        if self.monthIndex > 11:
+            self.monthIndex = 0
+            self.year += 1
+        self.month = self.monthList[self.monthIndex]
+
+
+
+def MainMenu(character):
     while True:
         menuOption = input(Fore.YELLOW + '\nWelcome to Lit Term Trail!\n' + Fore.RESET + 'You may:\n1.) Travel the trail \n2.) Learn about the trail \n3.) Choose management options \n4.) End \nWhat is your choice? ')
 
@@ -57,10 +90,11 @@ def MainMenu():
                 dummyInput = input('At the Dalles, you can try navigating the ' + Fore.RED + 'Columbian River' + Fore.RESET + ', \nbut if running the rapids with makeshift raft makes you queasy, better pay for the ' + Fore.RED + 'Barlow Road' + Fore.RESET + '.')
                 dummyInput = input('If for some reason you don\'t survive \n' + Fore.RED + '-- your wagon burns, or thieves steals your oxen, or you run out of provisions, or you die of cholera --' + Fore.RESET + ' \ndon\'t give up! Try again... and again... \nuntil your name is up with the others in history.')
             case '3': 
-                managementOptions = input(Fore.YELLOW + '\nManagement Options!\n' + Fore.RESET + 'You may:\n1.) Leave \nWhat is your choice? ')
-                while managementOptions not in ['1']: managementOptions = input(InvalidOption)
+                managementOptions = input(Fore.YELLOW + '\nManagement Options!\n' + Fore.RESET + f'You may:\n1.) Leave \n2.) Toggle Wait Time (currently {Wait})\nWhat is your choice? ')
+                while managementOptions not in ['1', '2']: managementOptions = input(InvalidOption)
                 match managementOptions:
                     case '1': pass
+                    case '2': ToggleWait()
             case '4': sys.exit(Fore.RED + 'Game Stopped' + Fore.RESET)
 
 
@@ -91,23 +125,35 @@ def SelectCharacterNames(character):
 
 
 
-def MonthSelection():
+def MonthSelection(date):
     month = ''
     while month not in ['1', '2', '3', '4', '5']: 
         month = input(Fore.YELLOW + '\nMonth Selection' + Fore.RESET + '\nIt is 1848. Your jumping off place for Oregon is Independence Missouri. You must decide which month to leave Independence.\n1.) March \n2.) April \n3.) May \n4.) June \n5.) July \n6.) Ask for advice\nWhat is your choice? ')
         if month == '6':
             dummyInput = input(Fore.GREEN + '\n[Press Enter to continue]' + Fore.RESET + 'You attend for public meeting for "folks with the California - Oregon fever" You\'re told:\n\nIf you leave too early, there won\'t be any grass for your oxen to eat. If you leave too late, you may not get to Oregon before winter comes. If you leave at just the right time, there will be green grass and the weather will still be cool.')
     match month:
-        case '1': month = 'March'
-        case '2': month = 'April'
-        case '3': month = 'may'
-        case '4': month = 'June'
-        case '5': month = 'July'
-    return month
+        case '1': date.monthIndex = 2
+        case '2': date.monthIndex = 3
+        case '3': date.monthIndex = 4
+        case '4': date.monthIndex = 5
+        case '5': date.monthIndex = 6
+    date.updateMonth()
 
 
 
-def FirstShop(difficulty, month, supplies):
+def DayTick(date, n=1, update=False):
+    date.increaseDay(n, update)
+    #WIP, food, conditions, etc tick
+
+
+
+def ToggleWait():
+    if Wait == True: Wait = False
+    else: Wait = True
+
+
+
+def FirstShop(difficulty, date, supplies):
     match difficulty:
         case '1': balance = 1600.0
         case '2': balance = 800.0
@@ -121,7 +167,7 @@ def FirstShop(difficulty, month, supplies):
 
 
     while True:
-        shopOption = input(Fore.YELLOW + f'\nBitmoji Store\nIndependence, Missouri\n{month} 1, 1848' + Fore.RESET + f'\n0.) Leave Store\n1.) Oxen: ${oxenCost}0 \n2.) Knowledge: ${knowledgeCost}0 \n3.) Clothing ${clothingCost}0 \n4.) Tokens: ${tokenCost}0 \n5.) Spare Parts: ${partsCost}0\n=============================\nTotal Bill: ${totalCost}0\n\nAmount you have: ${balance}0\nWhich item would you like to buy? ')
+        shopOption = input(Fore.YELLOW + f'\nBitmoji Store\nIndependence, Missouri\n{date.month} {date.day}, {date.year}' + Fore.RESET + f'\n0.) Leave Store\n1.) Oxen: ${oxenCost}0 \n2.) Knowledge: ${knowledgeCost}0 \n3.) Clothing ${clothingCost}0 \n4.) Tokens: ${tokenCost}0 \n5.) Spare Parts: ${partsCost}0\n=============================\nTotal Bill: ${totalCost}0\n\nAmount you have: ${balance}0\nWhich item would you like to buy? ')
 
         match shopOption:
             case '0':
@@ -152,7 +198,7 @@ def FirstShop(difficulty, month, supplies):
                 supplies.knowledge = int(knowledgeAmount)
 
             case '3':
-                clothingAmount = input(Fore.CYAN + f'\nBitmoji Store\nIndependence, Missouri' + Fore.YELLOW + f'\nBill so far: ${totalCost}0' + Fore.RESET + 'nYou\'ll need warm clothing in the mountains. \nI recommend taking at least 2 sets of clothes per person. \nEach set is $10.00. \nHow many sets of clothes do you want? (160 max) ')
+                clothingAmount = input(Fore.CYAN + f'\nBitmoji Store\nIndependence, Missouri' + Fore.YELLOW + f'\nBill so far: ${totalCost}0' + Fore.RESET + '\nYou\'ll need warm clothing in the mountains. \nI recommend taking at least 2 sets of clothes per person. \nEach set is $10.00. \nHow many sets of clothes do you want? (160 max) ')
                 while True:
                     if clothingAmount.isdigit() == False: clothingAmount = input(InvalidOption)
                     elif float(clothingAmount) > 160 or float(clothingAmount) < 0: clothingAmount = input(InvalidOption)
@@ -186,6 +232,64 @@ def FirstShop(difficulty, month, supplies):
     dummyInput = input(Fore.GREEN + '\n[Press Enter to continue]' + Fore.RESET + f'\nWell then, you\'re ready to start. Good Luck! \nYou have a long and difficult journey ahead of you.')
 
 
+def Trade(supplies):
+    wantType, wantAmt, giveType, giveAmt, tradable = '', '', '', '', True
+    for i in range (2):
+        type = random.randint(1,7)
+        match type:
+            case 1: 
+                type, amt = 'oxen', random.randint(1,3)
+                if supplies.oxen < amt: tradable = False
+            case 2: 
+                type, amt = 'sets of clothing', random.randint(1,5)
+                if supplies.clothing < amt: tradable = False
+            case 3: 
+                type, amt = 'tokens', random.randint(30,200)
+                if supplies.tokens < amt: tradable = False
+            case 4: 
+                type, amt = 'wagon wheels', random.randint(1,2)
+                if supplies.wheels < amt: tradable = False
+            case 5: 
+                type, amt = 'wagon axles', random.randint(1,2)
+                if supplies.axles < amt: tradable = False
+            case 6: 
+                type, amt = 'wagon tongues', random.randint(1,2)
+                if supplies.tongues < amt: tradable = False
+            case 7: 
+                type, amt = 'knowledge points', random.randint(30,200)
+                if supplies.knowledge < amt: tradable = False
+
+        if i == 0: wantType, wantAmt = type, amt
+        if i == 1: giveType, giveAmt = type, amt
+    
+
+    print(Fore.YELLOW + '\nYour Supplies:' + Fore.RESET + f'\noxen: {supplies.oxen}\nsets of clothing: {supplies.clothing}\ntokens: {supplies.tokens}\nwagon wheels: {supplies.wheels}\nwagon axles: {supplies.axles}\nwagon tongues: {supplies.tongues}\npoints of knowledge: {supplies.knowledge}\n\nYou meet another emigrant who wants ' + Fore.RED + f'-{wantAmt} {wantType}' + Fore.RESET + '.', end='')
+    if tradable == False: dummyInput = input(' You don\'t have this.\n' + Fore.GREEN + '[Press Enter to continue]' + Fore.RESET)
+    else: 
+        confirmTrade = input(' He will trade you' + Fore.GREEN + f' +{giveAmt} {giveType}' + Fore.RESET + '.\nAre you willing to trade? ') 
+        while confirmTrade not in ['y', 'n']: confirmTrade = input(InvalidOption)
+        if confirmTrade == 'y': 
+            match wantType:
+                case 'oxen': supplies.oxen -= wantAmt
+                case 'sets of clothing': supplies.clothing -= wantAmt
+                case 'tokens': supplies.tokens -= wantAmt
+                case 'wagon wheels': supplies.wheels -= wantAmt
+                case 'wagon axles': supplies.axles -= wantAmt
+                case 'wagon tongues': supplies.tongues -= wantAmt
+                case 'knowledge points': supplies.knowledge -= wantAmt
+            match giveType:
+                case 'oxen': supplies.oxen += giveAmt
+                case 'sets of clothing': supplies.clothing += giveAmt
+                case 'tokens': supplies.tokens += giveAmt
+                case 'wagon wheels': supplies.wheels += giveAmt
+                case 'wagon axles': supplies.axles += giveAmt
+                case 'wagon tongues': supplies.tongues += giveAmt
+                case 'knowledge points': supplies.knowledge += giveAmt
+            print(Fore.YELLOW + '\nYour Supplies:' + Fore.RESET + f'\noxen: {supplies.oxen}\nsets of clothing: {supplies.clothing}\ntokens: {supplies.tokens}\nwagon wheels: {supplies.wheels}\nwagon axles: {supplies.axles}\nwagon tongues: {supplies.tongues}\npoints of knowledge: {supplies.knowledge}')
+            if Wait == True: time.sleep(1)
+
+
+    
 
 def CalculateHealth(character):
     avgHealth = 0
@@ -200,12 +304,12 @@ def CalculateHealth(character):
     return health 
 
 
-def Menu(character, supplies, month):
+def Menu(character, supplies, date):
     health = CalculateHealth(character)
     weather = 'cool' #WIP
 
     while True:
-        print(Fore.MAGENTA + f'\nIndependence\n{month} 1, 1848' + Fore.RESET)
+        print(Fore.MAGENTA + f'\nIndependence\n{date.month} {date.day}, {date.year}' + Fore.RESET)
         print(Fore.YELLOW + f'Weather: {weather}\nHealth: {health}\nPace: {character.pace}\nReading Rate: {character.readingRate}' + Fore.RESET)
         option = input('1.) Continue on the trail\n2.) Check supplies\n3.) Change pace\n4.) Change reading rate\n5.) Stop to rest\n6.) Attempt to trade\n7.) Read for knowledge\nWhat is your choice? ')
         while option not in ['1', '2', '3', '4', '5', '6', '7']: option = input(InvalidOption)
@@ -236,10 +340,11 @@ def Menu(character, supplies, month):
 
             case '5':
                 rest = input('\nHow many days would you like to rest? ')
-                #WIP
+                DayTick(date, int(rest), True)
 
             case '6':
-                pass #WIP
+                Trade(supplies)
+                DayTick(date)
 
             case '7':
                 pass #WIP
@@ -248,18 +353,21 @@ def Menu(character, supplies, month):
 
 def main():
     global InvalidOption
+    global Wait
     InvalidOption = 'Please selection a available option: '
+    Wait = True
     character = Characters()
     supplies = Supplies()
+    date = Date()
 
-    MainMenu()
+    MainMenu(character)
     difficulty = SelectDifficulty()
     SelectCharacterNames(character)
-    month = MonthSelection()
-    FirstShop(difficulty, month, supplies)
+    MonthSelection(date)
+    FirstShop(difficulty, date, supplies)
 
     while True:
-        Menu(character, supplies, month)
+        Menu(character, supplies, date)
     
 
 
